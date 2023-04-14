2b2eceddb7279581a0eea2e8df33cb226367a4fb
mabulgu <abulgu@redhat.com>
2023-04-14 13:35:43 +0300

    remove unnecessary constraints

--- src/main/java/com/systemcraftsman/kubegame/service/WorldService.java
+++ src/main/java/com/systemcraftsman/kubegame/service/WorldService.java
@@ -35,13 +35,13 @@ public class WorldService {
     public synchronized void createWorldTableIfNotExists(Game game) {
         postgresService.execute(gameService.getPostgresServiceName(game) + ":" + GameService.POSTGRES_DB_PORT,
                 "postgres", game.getSpec().getDatabase().getUsername(), game.getSpec().getDatabase().getPassword(),
-                "CREATE TABLE IF NOT EXISTS World (name VARCHAR(50), game VARCHAR(50), description VARCHAR(1000), PRIMARY KEY (name, game))");
+                "CREATE TABLE IF NOT EXISTS World (name VARCHAR(50), game VARCHAR(50), description VARCHAR(1000), PRIMARY KEY (name))");
     }
 
     public void createWorldRecordIfNotExists(World world, Game game) {
         postgresService.execute(gameService.getPostgresServiceName(game) + ":" + GameService.POSTGRES_DB_PORT,
                 "postgres", game.getSpec().getDatabase().getUsername(), game.getSpec().getDatabase().getPassword(),
-                "INSERT INTO World (name, game, description) VALUES (?, ?, ?) ON CONFLICT (name, game) DO NOTHING",
+                "INSERT INTO World (name, game, description) VALUES (?, ?, ?) ON CONFLICT (name) DO NOTHING",
                 world.getMetadata().getName(),
                 world.getSpec().getGame(),
                 world.getSpec().getDescription());
