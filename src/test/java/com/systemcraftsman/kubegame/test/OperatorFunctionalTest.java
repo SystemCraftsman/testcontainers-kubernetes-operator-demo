51ef6398f89c26f41437be3f8f7542a5c7325c82
Aykut M. Bulgu <aykut@systemcraftsman.com>
2023-04-20 13:19:59 +0300

    Update OperatorFunctionalTest.java

--- src/test/java/com/systemcraftsman/kubegame/test/OperatorFunctionalTest.java
+++ src/test/java/com/systemcraftsman/kubegame/test/OperatorFunctionalTest.java
@@ -43,14 +43,14 @@ public class OperatorFunctionalTest {
     @Inject
     PostgresService postgresService;
 
-    //TODO: Start the operator instance
     @BeforeAll
     void startOperator() {
+        operator.start();
     }
 
-    //TODO: Stop the operator instance
     @AfterAll
     void stopOperator() {
+        operator.stop();
     }
 
     //TODO: Add a test for a Game object creation and assert its status and the dependants
